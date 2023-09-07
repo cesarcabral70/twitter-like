d827c01d3d0c83c0e00b16f8d3f73f8624494f3c
Cesar Cabral <cesarcabral@cesarcabral.com>
2023-09-07 22:14:38 +0200

    feature: Add tweets with 30s rules

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,59 @@
+"use client"
+
+import { Tweet, tweets$ } from "./../store/tweets"
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [data, setData] = useState<Tweet[]>([])
+
+  useEffect(() => {
+    const observable = tweets$
+
+    const subscription = observable.pipe().subscribe((data) => {
+      setData((prevDataArray: Tweet[]) => [...prevDataArray, data])
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [tweets$])
+
   return (
-    <div className="border-2 border-solid border-red-100 p-4 w-full max-w-screen-md">
-      Main content
+    <div className="p-4 w-full max-w-screen-md">
+      <ul>
+        {data
+          .sort((a, b) => b.timestamp - a.timestamp)
+          .map((item: Tweet, index: number) => {
+            const postTime = convertTimeToSec(item.timestamp)
+
+            if (postTime > 31) return null
+
+            return (
+              <li
+                className="p-4 bg-white mb-4 rounded-lg border-2 border-solid border-gray-200"
+                key={index}
+              >
+                <div>
+                  <strong className="mr-1 text-blue-800">
+                    @{item.account}
+                  </strong>
+                  <em className="text-gray-400">
+                    {postTime === 0 ? "Just now" : `${postTime}s ago`}
+                  </em>
+                </div>
+                {item.content}
+              </li>
+            )
+          })}
+      </ul>
     </div>
   )
+}
+
+function convertTimeToSec(data: number) {
+  const now = Date.now()
+  const timeDiff = now - data
+  const seconds = Math.floor(timeDiff / 1000)
+
+  return seconds
 }
---
+++ store/tweets.ts
@@ -0,0 +1,36 @@
+﻿// rxjs is exposed by
+// <https://cdnjs.cloudflare.com/ajax/libs/rxjs/6.4.0/rxjs.umd.min.js>
+
+import { BehaviorSubject, interval, map, merge } from "rxjs"
+
+export interface Tweet {
+  account: string
+  timestamp: number
+  content: string
+}
+
+const createTweetSource = (
+  frequency: number,
+  account: string,
+  attribute: string
+) => {
+  return interval(frequency).pipe(
+    map((i) => ({
+      account,
+      timestamp: Date.now(),
+      content: `${attribute} Tweet number ${i + 1}`,
+    }))
+  )
+}
+
+export const tweets$ = merge(
+  createTweetSource(5000, "AwardsDarwin", "Facepalm"),
+  createTweetSource(3000, "iamdevloper", "Expert"),
+  createTweetSource(5000, "CommitStrip", "Funny")
+)
+
+export const rawTweets$: any = new BehaviorSubject([])
+
+// tweets$.subscribe(console.log.bind(console))
+
+tweets$.subscribe()
